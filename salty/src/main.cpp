e083d6d52af52ffda7f19f5f397b14dd0173a6f9
Pocol <pocol.asura@gmail.com>
2013-06-07 02:53:15 +0900

    ・ファイル名を変更。 ・不足ファイルをコミット。

--- salty/src/main.cpp
+++ salty/src/main.cpp
@@ -12,16 +12,18 @@
 // Include
 //--------------------------------------------------------------------------------------------
 #include <crtdbg.h>
-#include "SampleApp.h"
+#include "SaltyApp.h"
 
 //--------------------------------------------------------------------------------------------
 //! @brief      メインエントリーポイントです.
 //--------------------------------------------------------------------------------------------
 int main( int argc, char** argv )
 {
+#if defined(DEBUG) || defined(_DEBUG)
     _CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+#endif//defined(DEBUG) || defined(_DEBUG)
     {
-        SampleApplication app;
+        SaltyApp app;
 
         app.Run();
     }

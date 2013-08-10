ec66d7691ec323bbf95b6c29f519f2baaa61f3b6
Pocol <project.asura@gmail.com>
2013-08-10 22:20:14 +0900

    コメント追加。

--- salty/s3d/src/s3d_bvh.cpp
+++ salty/s3d/src/s3d_bvh.cpp
@@ -12,6 +12,9 @@
 
 namespace /* anonymous */ {
 
+//---------------------------------------------------------------------------
+//      分割します.
+//---------------------------------------------------------------------------
 s32 Split( s3d::IShape** ppShapes, s32 size, f64 pivotVal, s32 axis )
 {
     s3d::BoundingBox bbox;
@@ -20,12 +23,18 @@ s32 Split( s3d::IShape** ppShapes, s32 size, f64 pivotVal, s32 axis )
 
     for( s32 i=0; i<size; ++i )
     {
+        // AABBを取得.
         bbox = ppShapes[i]->GetAABB();
+
+        // 中心を取得.
         centroid = ( bbox.min.v[axis] + bbox.max.v[axis] ) / 2.0;
+
+        // ピボットと比較.
         if ( centroid < pivotVal )
         {
             s3d::IShape* pTemp = ppShapes[ i ];
-            ppShapes[ i ] = ppShapes[ result ];
+
+            ppShapes[ i ]      = ppShapes[ result ];
             ppShapes[ result ] = pTemp;
             result++;
         }
@@ -42,18 +51,31 @@ s32 Split( s3d::IShape** ppShapes, s32 size, f64 pivotVal, s32 axis )
 
 namespace s3d {
 
+////////////////////////////////////////////////////////////////////////////
+// BVH structure
+////////////////////////////////////////////////////////////////////////////
+
+//--------------------------------------------------------------------------
+//      コンストラクタです.
+//--------------------------------------------------------------------------
 BVH::BVH()
 : aabb  ()
 , pLeft ( nullptr )
 , pRight( nullptr )
 { /* DO_NOTHING */ }
 
+//--------------------------------------------------------------------------
+//      引数付きコンストラクタです.
+//--------------------------------------------------------------------------
 BVH::BVH( IShape* pShape1, IShape* pShape2, const BoundingBox& bbox )
 : aabb  ( bbox )
 , pLeft ( pShape1 )
 , pRight( pShape2 )
 { /* DO_NOTHING */ }
 
+//--------------------------------------------------------------------------
+//      引数付きコンストラクタです.
+//--------------------------------------------------------------------------
 BVH::BVH( IShape* pShape1, IShape* pShape2 )
 : pLeft ( pShape1 )
 , pRight( pShape2 )
@@ -61,6 +83,9 @@ BVH::BVH( IShape* pShape1, IShape* pShape2 )
     aabb = BoundingBox::Merge( pShape1->GetAABB(), pShape2->GetAABB() );
 }
 
+//--------------------------------------------------------------------------
+//      初期化処理を行います.
+//--------------------------------------------------------------------------
 void BVH::Init( IShape** ppShapes, const s32 numShapes )
 {
     if ( numShapes == 1 )
@@ -80,6 +105,9 @@ void BVH::Init( IShape** ppShapes, const s32 numShapes )
     pRight = BuildBranch( &ppShapes[ midPoint ], numShapes - midPoint, 1 );
 }
 
+//--------------------------------------------------------------------------
+//      衝突判定を行います.
+//--------------------------------------------------------------------------
 bool BVH::IsHit( const Ray& ray, const f64 mini, const f64 maxi, ShadeRec& shadeRec ) const
 {
     if ( !aabb.IsHit( ray, mini, maxi ) )
@@ -95,9 +123,15 @@ bool BVH::IsHit( const Ray& ray, const f64 mini, const f64 maxi, ShadeRec& shade
     return ( isHit1 || isHit2 );
 }
 
+//--------------------------------------------------------------------------
+//      軸平行境界箱を取得します.
+//--------------------------------------------------------------------------
 BoundingBox BVH::GetAABB() const
 { return aabb; }
 
+//--------------------------------------------------------------------------
+//      ブランチを構築します.
+//--------------------------------------------------------------------------
 IShape* BVH::BuildBranch( IShape** ppShapes, const s32 numShapes, s32 axis )
 {
     if ( numShapes == 1 ) 
@@ -113,7 +147,7 @@ IShape* BVH::BuildBranch( IShape** ppShapes, const s32 numShapes, s32 axis )
 
     s32 midPoint = Split( ppShapes, numShapes, pivot.v[axis], axis );
 
-    IShape* left = BuildBranch( ppShapes, midPoint, (axis + 1) % 3 );
+    IShape* left  = BuildBranch( ppShapes, midPoint, (axis + 1) % 3 );
     IShape* right = BuildBranch( &ppShapes[midPoint], numShapes - midPoint, (axis + 1 ) % 3 );
 
     return new BVH( left, right, bbox );

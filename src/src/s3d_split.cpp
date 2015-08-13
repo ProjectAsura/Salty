b7cd0ab2402bab109b104d1758e5e9a9f197f59b
Pocol <project.asura@gmail.com>
2015-08-13 12:28:22 +0900

    メディアンスプリットを実装。

--- src/src/s3d_split.cpp
+++ src/src/s3d_split.cpp
@@ -14,6 +14,78 @@
 namespace s3d {
 
 //-------------------------------------------------------------------------------------------------
+//      中間値による分割を行います.
+//-------------------------------------------------------------------------------------------------
+bool MedianSplit( std::vector<IShape*> shapes, u32& bestIndex, u32& bestAxis )
+{
+    if ( shapes.size() <= 2 )
+    { return false; }
+
+    auto bestCost = F32_MAX;
+    bestAxis  = U32_MAX;
+    bestIndex = U32_MAX;
+
+    for( auto axis = 0; axis < 3; ++axis )
+    {
+        std::vector<IShape*> axisSortedL(shapes);
+        std::vector<IShape*> axisSortedR;
+
+        // 現在の軸においてボックスの重心座標を用いてソートを行う.
+        std::sort(
+            axisSortedL.begin(), 
+            axisSortedL.end(),
+            [axis](const IShape* l, const IShape* r)
+            {
+                auto boxL = l->GetBox();
+                auto boxR = r->GetBox();
+                assert( !boxL.isEmpty );
+                assert( !boxR.isEmpty );
+                auto cl = ( boxL.maxi.a[axis] + boxL.mini.a[axis] ) * 0.5f;
+                auto cr = ( boxR.maxi.a[axis] + boxR.mini.a[axis] ) * 0.5f;
+                return cl < cr;
+            }
+        );
+
+        auto idx = axisSortedL.size() / 2 - 1;
+        assert( idx >= 0 );
+        auto select = axisSortedL[idx];
+        auto left   = axisSortedL[0];
+        auto right  = axisSortedL[axisSortedL.size() - 1];
+        auto dist   = ( select->GetCenter() - left->GetCenter () ).LengthSq()
+                    + ( select->GetCenter() - right->GetCenter() ).LengthSq();
+
+        if ( dist < bestCost )
+        {
+            bestCost  = dist;
+            bestIndex = idx;
+            bestAxis  = axis;
+        }
+    }
+
+    // 葉ノードよりも良いパーティションが見つからなかった.
+    if ( bestAxis == U32_MAX )
+    { return false; }
+
+    // "最良の軸" で S をソートする.
+    std::sort( 
+        shapes.begin(),
+        shapes.end(),
+        [bestAxis]( const IShape* l, const IShape* r)
+        {
+            auto boxL = l->GetBox();
+            auto boxR = r->GetBox();
+            assert( !boxL.isEmpty );
+            assert( !boxR.isEmpty );
+            auto cl = ( boxL.maxi.a[bestAxis] + boxL.mini.a[bestAxis] ) * 0.5f;
+            auto cr = ( boxR.maxi.a[bestAxis] + boxR.mini.a[bestAxis] ) * 0.5f;
+            return cl < cr;
+        }
+    );
+
+    return true;
+}
+
+//-------------------------------------------------------------------------------------------------
 //      SAHによる分割を行います.
 //-------------------------------------------------------------------------------------------------
 bool SahSplit( std::vector<IShape*> shapes, u32& bestIndex, u32& bestAxis )
